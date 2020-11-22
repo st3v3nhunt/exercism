cf72957224524d5cb95b524fe71c7ce5a03fd5e4
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2020-11-22 23:07:44 +0000

    refactor: no need to unwrap value

--- rust/matching-brackets/src/lib.rs
+++ rust/matching-brackets/src/lib.rs
@@ -1,37 +1,22 @@
 pub fn brackets_are_balanced(string: &str) -> bool {
-    let mut stack: Vec<char> = vec![];
+    let mut stack: Vec<char> = Vec::new();
 
     for c in string.chars() {
-        println!("char: {}, vec: {:?}", c, stack);
+        // println!("char: {}, vec: {:?}", c, stack);
         match c {
             '[' | '{' | '(' => stack.push(c),
             ']' => {
-                if stack.len() > 0 {
-                    let a = stack.pop().unwrap();
-                    if a != '[' {
-                        return false;
-                    }
-                } else {
+                if stack.pop() != Some('[') {
                     return false;
                 }
             }
             '}' => {
-                if stack.len() > 0 {
-                    let a = stack.pop().unwrap();
-                    if a != '{' {
-                        return false;
-                    }
-                } else {
+                if stack.pop() != Some('{') {
                     return false;
                 }
             }
             ')' => {
-                if stack.len() > 0 {
-                    let a = stack.pop().unwrap();
-                    if a != '(' {
-                        return false;
-                    }
-                } else {
+                if stack.pop() != Some('(') {
                     return false;
                 }
             }
