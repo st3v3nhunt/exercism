142013036ee7e60c60f1e567541de3c44c69f742
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2020-11-22 12:43:17 +0000

    refactor: no need for 0 match

--- rust/grains/src/lib.rs
+++ rust/grains/src/lib.rs
@@ -1,6 +1,5 @@
 pub fn square(s: u32) -> u64 {
     match s {
-        0 => panic!("Square must be between 1 and 64"),
         1..=64 => (2 as u64).pow(s - 1),
         _ => panic!("Square must be between 1 and 64"),
     }
