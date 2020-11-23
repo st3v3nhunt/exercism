c2e5b56cc53502209eedff60beb98bf9da285036
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2020-11-23 22:00:22 +0000

    refactor: use recursion and Option map function

--- rust/collatz-conjecture/src/lib.rs
+++ rust/collatz-conjecture/src/lib.rs
@@ -1,14 +1,20 @@
 pub fn collatz(n: u64) -> Option<u64> {
-    let mut x = n;
-    let mut acc = 0;
-
-    while x != 1 {
-        acc += 1;
-        if x % 2 == 0 {
-            x = x / 2;
-        } else {
-            x = x * 3 + 1
-        }
+    match n {
+        0 => None,
+        1 => Some(0),
+        n if n % 2 == 0 => collatz(n / 2).map(|x| x + 1),
+        n => collatz(n * 3 + 1).map(|x| x + 1),
     }
-    Some(acc)
+    // let mut x = n;
+    // let mut acc = 0;
+
+    // while x != 1 {
+    //     acc += 1;
+    //     if x % 2 == 0 {
+    //         x = x / 2;
+    //     } else {
+    //         x = x * 3 + 1
+    //     }
+    // }
+    // Some(acc)
 }
