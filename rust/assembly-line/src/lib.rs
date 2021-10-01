bdd58c8f24febf1a5453460630897899ec02fae0
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2021-10-01 13:01:40 +0100

    feat(rust): assembly-line

---
+++ rust/assembly-line/src/lib.rs
@@ -0,0 +1,15 @@
+pub fn production_rate_per_hour(speed: u8) -> f64 {
+    let base_rate_per_hour = 221.0 * speed as f64;
+    let success_rate = match speed {
+        1..=4 => 1.0,
+        5..=8 => 0.9,
+        9 | 10 => 0.77,
+        _ => 0.0,
+    };
+
+    base_rate_per_hour * success_rate
+}
+
+pub fn working_items_per_minute(speed: u8) -> u32 {
+    (production_rate_per_hour(speed) / 60.0) as u32
+}
